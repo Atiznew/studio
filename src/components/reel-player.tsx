9355b0bdc65ad34e79e3934e4d827fd521e2d82f
Hitesh Roy <atozproductaser@gmail.com>
2026-01-04 18:09:30 +0000

    Ye jo aisa icon hai ye naa dikhe aisa kuchh sirf video dikhe baki kuchh

--- src/components/reel-player.tsx
+++ src/components/reel-player.tsx
@@ -106,7 +106,13 @@ export function ReelPlayer({ video, isIntersecting }: ReelPlayerProps) {
             className="react-player"
             config={{
                 youtube: {
-                    playerVars: { showinfo: 0, controls: 0, modestbranding: 1, rel: 0 }
+                    playerVars: { 
+                        showinfo: 0, 
+                        controls: 0, 
+                        modestbranding: 1, 
+                        rel: 0,
+                        iv_load_policy: 3
+                    }
                 },
                 facebook: {
                     appId: '12345'
